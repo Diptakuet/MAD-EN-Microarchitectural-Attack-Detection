d67cf984d5d76cd19134ed0e571a87fccc964996
Debopriya Roy Dipta <Diptakuet@users.noreply.github.com>
2022-06-28 17:51:25 -0500

    Update data_core.sh

--- Dataset/Benign_Application/Processor/jpegxl/Script/data_core.sh
+++ Dataset/Benign_Application/Processor/jpegxl/Script/data_core.sh
@@ -7,7 +7,7 @@ do
   pow=$(sudo cat /sys/devices/virtual/powercap/intel-rapl/intel-rapl\:0/intel-rapl\:0\:0/energy_uj) 
   sleep 0.0005
   pow1=$(sudo cat /sys/devices/virtual/powercap/intel-rapl/intel-rapl\:0/intel-rapl\:0\:0/energy_uj)
-  echo $(($pow1-$pow)) >> /home/Debopriya/medusa/Topic_3_Data_collection/All_Data/Useful_Resource/Benign_Application/Processor/jpegxl/Data/jpegxl_png_5_core_1_50.csv
+  echo $(($pow1-$pow)) >> ~/Dataset/Benign_Application/Processor/jpegxl/Data/jpegxl_png_5_core_1_50.csv
   sleep 0.0005
 done
 #end_time=$(date +%s)
